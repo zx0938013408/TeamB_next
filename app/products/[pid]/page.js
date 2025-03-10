5d115acb307f118de6a6efc946cfd6b8580004b2
yisichoco <yisichoco@gmail.com>
2025-03-10 15:22:41 +0800

    URL 相關的勾子

---
+++ app/products/[pid]/page.js
@@ -0,0 +1,24 @@
+"use client";
+
+import {
+  useParams,
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from "next/navigation";
+import React from "react";
+
+export default function ProductItemPage() {
+  const router = useRouter();
+  const params = useParams();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  return (
+    <div>
+      <h2>ProductItemPage</h2>
+      <p>pathname: {pathname}</p>
+      <p>params.pid: {params.pid}</p>
+      <p>searchParams.get('name'): {searchParams.get("name")}</p>
+    </div>
+  );
+}
--- app/products/page.js
+++ app/products/page.js
@@ -1,13 +1,23 @@
-'use client';
+"use client";
 import React from "react";
+import Link from "next/link";
 
 export default function ProductsPage() {
   // 不要直接使用前端的功能, 應該在 useEffect() 裡使用
-  // console.log('ProductsPage', location.href); 
-  
+  // console.log('ProductsPage', location.href);
+
   return (
     <div>
       <h1>產品頁</h1>
+      <p>
+        <Link href="/products/12?name=產品一">/products/12?name=產品一</Link>
+      </p>
+      <p>
+        <Link href="/products/15?name=產品A">/products/15?name=產品A</Link>
+      </p>
+      <p>
+        <Link href="/products/18?name=產品8">/products/18?name=產品8</Link>
+      </p>
     </div>
   );
 }
