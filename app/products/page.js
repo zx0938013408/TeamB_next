862faa67b14add21e63e3f1baa7195610d54248e
yisichoco <yisichoco@gmail.com>
2025-03-10 15:11:18 +0800

    前端的功能, 應該在 useEffect() 裡使用

--- app/products/page.js
+++ app/products/page.js
@@ -1,6 +1,10 @@
+'use client';
 import React from "react";
 
 export default function ProductsPage() {
+  // 不要直接使用前端的功能, 應該在 useEffect() 裡使用
+  // console.log('ProductsPage', location.href); 
+  
   return (
     <div>
       <h1>產品頁</h1>
