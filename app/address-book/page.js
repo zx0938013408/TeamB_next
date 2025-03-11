0a953b53665bcf5aa1c07acdaf965c8a9ab7e105
Shinder Lin <shinder.lin@gmail.com>
2025-03-11 10:33:28 +0800

    把列表資料放到狀態

--- app/address-book/page.js
+++ app/address-book/page.js
@@ -4,13 +4,31 @@ import { useState, useEffect } from "react";
 import { AB_LIST } from "@/config/api-path";
 
 export default function ABListPage() {
+  const [listData, setListData] = useState({
+    success: false,
+    perPage: 0,
+    totalRows: 0,
+    totalPages: 0,
+    page: 0,
+    rows: [],
+    keyword: "",
+  });
+
   useEffect(() => {
     fetch(AB_LIST)
       .then((r) => r.json())
       .then((obj) => {
         console.log(obj);
+        if (obj.success) {
+          setListData(obj);
+        }
       });
   }, []);
 
-  return <div>ABListPage</div>;
+  console.log(listData);
+  return (
+    <div>
+      <pre>{JSON.stringify(listData, null, 4)}</pre>
+    </div>
+  );
 }
