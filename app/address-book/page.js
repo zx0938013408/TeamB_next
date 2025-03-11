a41854daf6da617b123bd599dd849452727cdf4c
Shinder Lin <shinder.lin@gmail.com>
2025-03-11 10:55:19 +0800

    以表格呈現列表資料

--- app/address-book/page.js
+++ app/address-book/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { AB_LIST } from "@/config/api-path";
+import { AB_LIST, AVATAR_PATH } from "@/config/api-path";
 
 export default function ABListPage() {
   const [listData, setListData] = useState({
@@ -27,8 +27,47 @@ export default function ABListPage() {
 
   console.log(listData);
   return (
-    <div>
-      <pre>{JSON.stringify(listData, null, 4)}</pre>
-    </div>
+    <>
+      <div className="row">
+        <div className="col">
+          <table className="table table-bordered table-striped">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>姓名</th>
+                <th>頭貼</th>
+                <th>電郵</th>
+                <th>手機</th>
+                <th>生日</th>
+                <th>地址</th>
+              </tr>
+            </thead>
+            <tbody>
+              {listData.rows.map((r, i) => {
+                return (
+                  <tr key={i}>
+                    <td>{r.ab_id}</td>
+                    <td>{r.name}</td>
+                    <td>
+                      {r.avatar ? (
+                        <img
+                          src={`${AVATAR_PATH}/${r.avatar}`}
+                          alt=""
+                          height="100"
+                        />
+                      ) : null}
+                    </td>
+                    <td>{r.email}</td>
+                    <td>{r.mobile}</td>
+                    <td>{r.birthday}</td>
+                    <td>{r.address}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   );
 }
