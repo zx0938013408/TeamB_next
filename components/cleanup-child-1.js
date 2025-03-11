fffb14b040674b1ebc08129b6f4a383bb550b64c
Shinder Lin <shinder.lin@gmail.com>
2025-03-11 14:31:10 +0800

    使用 setInterval 時要記得清除的動作

--- components/cleanup-child-1.js
+++ components/cleanup-child-1.js
@@ -1,16 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function CleanupChild1() {
+  const [num, setNum] = useState(0);
+
   useEffect(() => {
     console.log("CleanupChild1 mounted");
+
+    const interval_id = setInterval(() => {
+      console.log({ num });
+      setNum((old) => old + 1);
+    }, 300);
+
     return () => {
       console.log("CleanupChild1 will unmount");
+      clearInterval(interval_id); // 把計時器停下來
     };
   }, []);
 
   return (
     <>
-      <div>CleanupChild1</div>
+      <div>CleanupChild1 {num}</div>
     </>
   );
 }
