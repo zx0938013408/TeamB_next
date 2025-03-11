e9a88b9ce8d2a9cb7c5b1ca1c7d2a635e606ab32
Shinder Lin <shinder.lin@gmail.com>
2025-03-11 11:26:48 +0800

    用 searchParams 變動去帶動內容的載入

--- app/address-book/page.js
+++ app/address-book/page.js
@@ -1,9 +1,12 @@
 "use client";
-
+import Link from "next/link";
 import { useState, useEffect } from "react";
 import { AB_LIST, AVATAR_PATH } from "@/config/api-path";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function ABListPage() {
+  const searchParams = useSearchParams();
+
   const [listData, setListData] = useState({
     success: false,
     perPage: 0,
@@ -15,7 +18,7 @@ export default function ABListPage() {
   });
 
   useEffect(() => {
-    fetch(AB_LIST)
+    fetch(`${AB_LIST}${location.search}`)
       .then((r) => r.json())
       .then((obj) => {
         console.log(obj);
@@ -23,11 +26,30 @@ export default function ABListPage() {
           setListData(obj);
         }
       });
-  }, []);
+  }, [searchParams]);
 
   console.log(listData);
   return (
     <>
+      <div className="row">
+        <div className="col">
+          <nav aria-label="Page navigation example">
+            <ul className="pagination">
+              {Array(11)
+                .fill(1)
+                .map((v, i) => {
+                  return (
+                    <li className="page-item" key={i}>
+                      <Link className="page-link" href={`?page=${i + 1}`}>
+                        {i + 1}
+                      </Link>
+                    </li>
+                  );
+                })}
+            </ul>
+          </nav>
+        </div>
+      </div>
       <div className="row">
         <div className="col">
           <table className="table table-bordered table-striped">
