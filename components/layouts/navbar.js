5406303d0883569c116b0c9eb4996bc6e5bdb7b9
Shinder Lin <shinder.lin@gmail.com>
2025-03-12 14:57:10 +0800

    navbar 呈現登入的狀態

--- components/layouts/navbar.js
+++ components/layouts/navbar.js
@@ -1,8 +1,10 @@
 "use client";
 import React from "react";
 import Link from "next/link";
+import { useAuth } from "@/contexts/auth-context";
 
 export default function ABNavbar() {
+  const { auth, logout } = useAuth();
   return (
     <div className="container">
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -36,16 +38,38 @@ export default function ABNavbar() {
             </ul>
 
             <ul className="navbar-nav mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link" href="/address-book/quick-login">
-                  快速登入
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" href="/register">
-                  會員註冊
-                </Link>
-              </li>
+              {auth.id ? (
+                <>
+                  <li className="nav-item">
+                    <a className="nav-link">{auth.nickname}</a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link"
+                      href="#"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        logout();
+                      }}
+                    >
+                      登出
+                    </a>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <Link className="nav-link" href="/address-book/quick-login">
+                      快速登入
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" href="/register">
+                      會員註冊
+                    </Link>
+                  </li>
+                </>
+              )}
             </ul>
           </div>
         </div>
