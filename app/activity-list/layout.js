bdc0a153dab6f85818eddc82c9db386d1cf8b685
Chago <chago789563@gmail.com>
2025-03-17 15:14:25 +0800

    修改 nav 篩選列與麵包屑

--- app/activity-list/layout.js
+++ app/activity-list/layout.js
@@ -10,12 +10,6 @@ export default function RootLayout({ children }) {
     <>
       <html lang="en">
         <head>
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-          />
-          <link rel="styleIcons" href="public/TeamB_Icon/style.css" />
-
         </head>
         <body>
           {children}
