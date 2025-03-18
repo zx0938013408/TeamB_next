f1542a0e74e56a0072205c23b11962e56a67215a
Joyce <jlaccfordrive.gmail.com>
2025-03-19 02:31:58 +0800

    增加上方padding到150px

--- app/shop/page.js
+++ app/shop/page.js
@@ -1,16 +1,19 @@
 import styles from "./shop.module.css";
 import "@/public/TeamB_Icon/style.css";
-// import Card from "../../components/shop/card";
 import Link from "next/link";
 import Carousel from "../../components/shop/carousel";
-import Card from "@/components/shop/card";
+
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function ShopPage() {
   return (
     <>
+      <Header />
+
       <div className={styles.body}>
         <div className={styles.container}>
-        {/* TODO: 麵包屑、商城bar */}
+          {/* TODO: 麵包屑、商城bar */}
           {/* 搜尋欄 */}
           <div className={styles.search}>
             <span className={`icon-Search ${styles.iconSearch}`} />
@@ -20,7 +23,7 @@ export default function ShopPage() {
             <div className={styles.titleBg}>
               <div className={styles.title}>上衣</div>
             </div>
-            <Carousel categoryId={1}/>
+            <Carousel categoryId={1} />
             <div className={styles.more}>
               <div>
                 <Link href="../shop/top" style={{ textDecoration: "none" }}>
@@ -37,7 +40,7 @@ export default function ShopPage() {
             <div className={styles.titleBg}>
               <div className={styles.title}>褲類</div>
             </div>
-            <Carousel categoryId={2}/>
+            <Carousel categoryId={2} />
             <div className={styles.more}>
               <div>
                 <Link href="../shop/top" style={{ textDecoration: "none" }}>
@@ -54,7 +57,7 @@ export default function ShopPage() {
             <div className={styles.titleBg}>
               <div className={styles.title}>鞋類</div>
             </div>
-            <Carousel categoryId={3}/>
+            <Carousel categoryId={3} />
             <div className={styles.more}>
               <div>
                 <Link href="../shop/top" style={{ textDecoration: "none" }}>
@@ -71,7 +74,7 @@ export default function ShopPage() {
             <div className={styles.titleBg}>
               <div className={styles.title}>運動配件</div>
             </div>
-            <Carousel categoryId={4}/>
+            <Carousel categoryId={4} />
             <div className={styles.more}>
               <div>
                 <Link href="../shop/top" style={{ textDecoration: "none" }}>
@@ -85,6 +88,8 @@ export default function ShopPage() {
           </div>
         </div>
       </div>
+
+      <Footer />
     </>
   );
 }
--- app/shop/top/page.js
+++ app/shop/top/page.js
@@ -2,30 +2,36 @@ import styles from "./shop_top.module.css";
 import "@/public/TeamB_Icon/style.css";
 import Carousel from "../../../components/shop/carousel";
 import Card from "../../../components/shop/card";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function TopPage() {
   return (
-    <div className={styles.body}>
-      <div className={styles.container}>
-        {/* 搜尋欄 */}
-        <div className={styles.search}>
-          <span className={`icon-Search ${styles.iconSearch}`} />
-        </div>
-
-        <div className={styles.itemsSection}>
-          <div className={styles.titleBg}>
-            <div className={styles.title}>上衣</div>
+    <>
+      <Header />
+      <div className={styles.body}>
+        <div className={styles.container}>
+          {/* 搜尋欄 */}
+          <div className={styles.search}>
+            <span className={`icon-Search ${styles.iconSearch}`} />
           </div>
-          <Carousel categoryId={1} />
 
-          {/* <div className={styles.more}>
+          <div className={styles.itemsSection}>
+            <div className={styles.titleBg}>
+              <div className={styles.title}>上衣</div>
+            </div>
+            <Carousel categoryId={1} />
+
+            {/* <div className={styles.more}>
             <div className={styles.textBox}>
               <div className={styles.text}>查看更多</div>
               <span className={`icon-Right ${styles.iconRight}`} />
             </div>
           </div> */}
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
