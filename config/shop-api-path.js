03fdd65219693c7904b34334c44b57665fc4c20c
Joyce <jlaccfordrive.gmail.com>
2025-03-24 01:26:07 +0800

    修改取得圖片的路徑

--- config/shop-api-path.js
+++ config/shop-api-path.js
@@ -2,8 +2,8 @@
 export const API_SERVER = `http://localhost:3001`;
 // export const API_SERVER = `http://172.23.53.146:3001`;
 
-// 頭貼的路
-export const AVATAR_PATH = `${API_SERVER}/products/api`;
+// 商品圖的路
+export const AVATAR_PATH = `${API_SERVER}/imgs/`;
 
 // 取得通訊錄列表的資料 GET
 export const AB_LIST = `${API_SERVER}/products/api`;
